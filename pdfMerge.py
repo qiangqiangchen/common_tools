ccccde2a287e1aa99eb71d9700a35adf4850e7db
qiangqiangchen <330445748@qq.com>
2019-12-09 19:05:34 +0800

    增加图片转PDF

--- pdfMerge.py
+++ pdfMerge.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
 import os
 
+import img2pdf
 from PyPDF2 import PdfFileReader, PdfFileWriter
 
 """
@@ -10,11 +11,18 @@ from PyPDF2 import PdfFileReader, PdfFileWriter
 
 
 #获取给定目录下所有PDF文件，返回一个文件列表
-def getFileName(filepath):
+def getFileName(filepath,filter='pdf'):
     file_list = []
+    filter_list=[]
+    if isinstance(filter,str):
+        filter_list.append(filter)
+    else:
+        filter_list=filter
+    filter_list=list(map(lambda x:x.lower(),filter_list))
     for root, dirs, files in os.walk(filepath):
         for filepath in files:
-            if filepath.lower().endswith("pdf"):
+            #if filepath.lower().endswith(filter):
+            if filepath.lower().split('.')[-1] in filter_list:
                 file_list.append(os.path.join(root, filepath))
     return file_list
 
@@ -33,8 +41,18 @@ def pdfMerge(filepath, outfile):
             print("------>正在写入第{}页".format(page))
     output.write(open(outfile, 'wb'))
 
+def img_to_pdf(filepath, outfile):
+    img_list=getFileName(filepath,filter=['jpg','jpeg','png'])
+    a4input=(img2pdf.mm_to_pt(210),img2pdf.mm_to_pt(297))
+    layout_fun=img2pdf.get_layout_fun(a4input)
+    with open(outfile,'wb') as f:
+        f.write(img2pdf.convert(img_list,layout_fun=layout_fun))
+    print("{}写入完成".format(outfile))
 
 if __name__ == "__main__":
-    pdf_dir = r"XXXXX"
-    out_path = r"XXXX.pdf"
-    pdfMerge(pdf_dir, out_path)
+    img_dir=r'E:\pandownload\韩漫\中文合集8部\约炮APP 1-6\1'
+    #pdf_dir = r"XXXXX"
+    out_path = r"F:\piccc\pic.pdf"
+    # pdfMerge(pdf_dir, out_path)
+    # img_to_pdf(img_dir,out_path)
+
